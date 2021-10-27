8f44ebb7e4e75412c5e628654fa226e49b86d375
Achmad Mahfud <achmadmahfud01@gmail.com>
2021-10-28 02:02:37 +0700

    7

---
+++ Latihan Python.py
@@ -0,0 +1,23 @@
+print("hello")
+print("saya sedang belajar python")
+
+a = 8
+b = 6
+print("variabel a = ",a)
+print("variabel b = ",b)
+print("hasil penjumlahan a + b =", a+b)
+
+
+#bagian 2
+input("masukkan nilai a:")
+input("masukkan nilai b:")
+print("variabel a =",a)
+print("variabel b =",b)
+print("hasil penggabungan {1} & {0} = %d". format(a,b) %(a+b))
+
+
+# Konversi Nilai Variabel
+a=int(a)
+b=int(b)
+print("hasil penjumlahan {1}+{0}=%d". format(a,b) %(a+b))
+print("hasil pembagian {1}/{0}=%d". format(a,b) %(a/b))
